6ae0c5579043afc2f0f9865721bf7ffd1d3686f2
Sam Dension <sdenison@satx.rr.com>
2023-08-17 19:45:43 -0500

    SD - dynamo#8 - Add command line application.

---
+++ Source/Dynamo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+
+namespace Dynamo
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var rootLevelCommands = new RootLevelCommands();
+            var returnValue = await rootLevelCommands.InvokeAsync(args);
+            return returnValue;
+        }
+    }
+}
