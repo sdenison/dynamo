ca4216ef7783fd98f14eeff9fbc1ca0182dbc6e6
Sam Dension <sdenison@satx.rr.com>
2023-08-19 15:53:32 -0500

    SD - dynamo#8 - Bad update lambda aws cli call.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd dynamo-lambda-release
 zip -r $ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions.zip
 cp $ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions.zip $ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions-v$CODEBUILD_BUILD_NUMBER.zip
 aws s3 cp $ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions-v$CODEBUILD_BUILD_NUMBER.zip s3://$ENVIRONMENT_NAME-dynamo-deployment/
-aws lambda update-function-code --function-name $ENVIRONMENT_NAME-dynamo-dotnet-lambda --s3-bucket $$ENVIRONMENT_NAME-dynamo-deployment/$ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions-v$CODEBUILD_BUILD_NUMBER.zip
+aws lambda update-function-code --function-name $ENVIRONMENT_NAME-dynamo-dotnet-lambda --s3-bucket $ENVIRONMENT_NAME-dynamo-deployment --s3-key $ENVIRONMENT_NAME-dynamo-dontnet-lambda-functions-v$CODEBUILD_BUILD_NUMBER.zip
