9e975c5b5ffe2a7bdaac5545bf4f45e921d7c5ec
Sam Dension <sdenison@satx.rr.com>
2023-08-18 22:24:22 -0500

    SD - dynamo#8 - Adding the Lambda handler.

--- Source/Dynamo/Program.cs
+++ Source/Dynamo/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Dynamo
 {
-    internal class Program
+    public class Program
     {
         static async Task<int> Main(string[] args)
         {
@@ -10,5 +10,20 @@ namespace Dynamo
             var returnValue = await rootLevelCommands.InvokeAsync(args);
             return returnValue;
         }
+
+		//This is exposed so the Lambda functions have access to anything the console appliation does.
+		public static async Task InvokeAsync(string[] args)
+		{
+			try
+			{
+				//The next two lines are all that's needed for the full cli.
+				var newRoot = new RootLevelCommands();
+				await newRoot.InvokeAsync(args);
+			}
+			catch (Exception ex)
+			{
+				var x = ex.InnerException;
+			}
+		}
     }
 }
