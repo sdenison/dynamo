480affa12e8e86ac73df013fe744b5e6871520fe
Sam Dension <sdenison@satx.rr.com>
2023-09-22 19:01:24 -0500

    SD - dynamo#10 - Putting this on pause for the moment.

--- Source/Dynamo.Lambda/Function.cs
+++ Source/Dynamo.Lambda/Function.cs
@@ -1,4 +1,5 @@
-﻿using Amazon.Lambda.Core;
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using System.Text.RegularExpressions;
 
 //Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -8,21 +9,32 @@ namespace Dynamo.Lambda
 {
     public class Function
     {
-        public async Task<string> FunctionHandler(string input, ILambdaContext context)
-        {
-            var functionOutput = await FunctionInvoker(input, context);
-            return functionOutput;
+        public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {        
+            request.QueryStringParameters.TryGetValue("name", out var name);
+            name = name ?? "John Doe";
+            var message = $"Hello {name}, from AWS Lambda";
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                Body = message,
+                StatusCode = 200
+            };
         }
+        //public async Task<string> FunctionHandler(string input, ILambdaContext context)
+        //{
+        //    var functionOutput = await FunctionInvoker(input, context);
+        //    return functionOutput;
+        //}
 
-        public async Task<string> FunctionInvoker(string input, ILambdaContext context)
-        {
-            var newOut = new StringWriter();
-            Console.SetOut(newOut);
-            var args = Regex.Matches(input, @"[\""].+?[\""]|[^ ]+")
-                .Select(m => m.Value.Replace("\"", ""))
-                .ToArray();
-            await Program.InvokeAsync(args);
-            return newOut.ToString();
-        }
+        //public async Task<string> FunctionInvoker(string input, ILambdaContext context)
+        //{
+        //    var newOut = new StringWriter();
+        //    Console.SetOut(newOut);
+        //    var args = Regex.Matches(input, @"[\""].+?[\""]|[^ ]+")
+        //        .Select(m => m.Value.Replace("\"", ""))
+        //        .ToArray();
+        //    await Program.InvokeAsync(args);
+        //    return newOut.ToString();
+        //}
     }
 }
