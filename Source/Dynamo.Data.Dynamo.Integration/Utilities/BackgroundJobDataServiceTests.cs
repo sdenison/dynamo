37362524ac428af3ddf115a53f6a08f798e558da
Sam Dension <sdenison@satx.rr.com>
2023-10-01 15:57:53 -0500

    SD - dynamo#12 - Adding an integration test for business object to dynamoDb communication.

--- Source/Dynamo.Data.Dynamo.Integration/Utilities/BackgroundJobDataServiceTests.cs
+++ Source/Dynamo.Data.Dynamo.Integration/Utilities/BackgroundJobDataServiceTests.cs
@@ -1,8 +1,11 @@
 ﻿using Amazon;
 using Amazon.DynamoDBv2;
+using Csla;
+using Csla.Configuration;
 using Dynamo.Business.Shared.Utilities;
 using Dynamo.Business.Utilities;
 using Dynamo.Data.DynamoDb.Utilities;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using ServiceStack.Aws.DynamoDb;
 
@@ -30,6 +33,25 @@ namespace Dynamo.Data.DynamoDb.Integration.Utilities
             backgroundJob.JobStatus = JobStatus.Submitted;
             dataService.Update(backgroundJob);
             dataService.Delete(backgroundJob.Id);
+        }
+
+        [Test]
+        public void Use_csla_BackgroundJob_and_dynamo_together()
+        {
+            var awsDb = new AmazonDynamoDBClient(RegionEndpoint.USEast2);
+            var db = new PocoDynamo(awsDb);
+            db.RegisterTable<BackgroundJobEntity>();
+            var dataService = new BackgroundJobDataService(db);
+
+            var services = new ServiceCollection();
+            services.AddCsla();
+            services.AddTransient<IBackgroundJobDataService>(o => dataService);
+            var serviceProvider = services.BuildServiceProvider();
+            var portal = serviceProvider.GetRequiredService<IDataPortal<BackgroundJob>>();
+
+            var job = portal.Create();
+            Assert.IsTrue(job.IsNew);
+            job = job.Save();
         }
     }
 }
