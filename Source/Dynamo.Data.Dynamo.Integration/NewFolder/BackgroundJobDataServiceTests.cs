a33532e6f2d4beecabebf36e7bb75d9fb9bebdb1
Sam Dension <sdenison@satx.rr.com>
2023-10-01 15:34:35 -0500

    SD - dynamo#12 - Creating service that handles DynamoDb for BackgroundJobDataSerivce.

---
+++ Source/Dynamo.Data.Dynamo.Integration/NewFolder/BackgroundJobDataServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Dynamo.Business.Shared.Utilities;
+using Dynamo.Business.Utilities;
+using Dynamo.Data.Dynamo.Utilities;
+using NUnit.Framework;
+using ServiceStack.Aws.DynamoDb;
+
+namespace Dynamo.Data.Dynamo.Integration.NewFolder
+{
+    [TestFixture]
+    public class BackgroundJobDataServiceTests
+    {
+        [Test, Ignore("This integration test can be used to debug DynamoDb issues")]
+        public void Can_read_and_write_to_dynamodb()
+        {
+            var awsDb = new AmazonDynamoDBClient(RegionEndpoint.USEast2);
+            var db = new PocoDynamo(awsDb);
+            db.RegisterTable<BackgroundJobEntity>();
+            var dataService = new BackgroundJobDataService(db);
+            var backgroundJob = new BackgroundJobEntity()
+            {
+                Id = Guid.NewGuid(),
+                JobStatus = JobStatus.Initializing,
+                JobType = JobType.BusyBox,
+            };
+            dataService.Insert(backgroundJob);
+            var insertedEntity = dataService.Get(backgroundJob.Id);
+            backgroundJob.JobStatus = JobStatus.Submitted;
+            dataService.Update(backgroundJob);
+            dataService.Delete(backgroundJob.Id);
+        }
+    }
+}
---
+++ Source/Dynamo.Data.DynamoDb/Utilities/BackgroundJobDataService.cs
@@ -0,0 +1,39 @@
+﻿using Dynamo.Business.Shared.Utilities;
+using ServiceStack;
+using ServiceStack.Aws.DynamoDb;
+
+namespace Dynamo.Data.Dynamo.Utilities
+{
+    public class BackgroundJobDataService : Service, IBackgroundJobDataService
+    {
+        public IPocoDynamo Dynamo { get; set; }
+
+        public BackgroundJobDataService(IPocoDynamo dynamo)
+        {
+            Dynamo = dynamo;
+        }
+
+        public BackgroundJobEntity Get(Guid id)
+        {
+            return Dynamo.GetItem<BackgroundJobEntity>(id);
+        }
+
+        public BackgroundJobEntity Insert(BackgroundJobEntity entity)
+        {
+            Dynamo.PutItem(entity);
+            return entity;
+        }
+
+        public BackgroundJobEntity Update(BackgroundJobEntity entity)
+        {
+            Dynamo.PutItem(entity);
+            return entity;
+        }
+
+        public bool Delete(Guid id)
+        {
+            Dynamo.DeleteItem<BackgroundJobEntity>(id.ToString());
+            return true;
+        }
+    }
+}
