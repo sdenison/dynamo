43e751f17861c14a0e62e3d50f448958f44ed8c0
Sam Dension <sdenison@satx.rr.com>
2023-10-08 13:44:37 -0500

    SD - dynamo#13 - Very basic upload happening.

---
+++ Source/Dynamo.Ui.Blazor.Server/Controllers/FileUploadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dynamo.Ui.Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> PostFile(
+            [FromForm] IEnumerable<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                var x = file.Name;
+                return Ok();
+            }
+
+            return Ok();
+        }
+    }
+}
