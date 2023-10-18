41eb22ca0f16b1ce227484729049fb3996db3104
Sam Dension <sdenison@satx.rr.com>
2023-10-17 19:37:47 -0500

    SD - dynamo#19 - Small cleanup.

--- Source/Dynamo.Business.Shared/AdventOfCode/Guard/Guard.cs
+++ Source/Dynamo.Business.Shared/AdventOfCode/Guard/Guard.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Text;
 
 namespace Dynamo.Business.Shared.AdventOfCode.Guard
 {
