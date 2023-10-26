479f3645504ac5ad2967ee083035ad49c2d6c4d6
Sam Dension <sdenison@satx.rr.com>
2023-10-26 18:04:48 -0500

    SD - dynamo#21 - Very basics of a Node working.

---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Navigation/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamo.Business.Shared.AdventOfCode.Navigation
+{
+    public class Node
+    {
+        public List<Node> ChildNodes { get; } = new List<Node>();
+        public List<int> Metadata { get; } = new List<int>();
+
+        public Node(int[] nodeDefinition)
+        {
+            if (nodeDefinition[0] == 0)
+                for (var i = 0; i < nodeDefinition[1]; i++)
+                    Metadata.Add(nodeDefinition[2 + i]);
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/Navigation/NodeTests.cs
@@ -0,0 +1,19 @@
+﻿using Dynamo.Business.Shared.AdventOfCode.Navigation;
+using NUnit.Framework;
+
+namespace Dynamo.Business.Unit.Tests.AdventOfCode.Navigation
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        [Test]
+        public void Can_create_a_node_with_no_children()
+        {
+            var nodeDefinition = new[] { 0, 1, 99 };
+            var node = new Node(nodeDefinition);
+            Assert.AreEqual(1, node.Metadata.Count);
+            Assert.AreEqual(99, node.Metadata[0]);
+        }
+
+    }
+}
