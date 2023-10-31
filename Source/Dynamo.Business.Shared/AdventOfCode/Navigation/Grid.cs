d7d030253ce92b2a3051f3e9c7fa34a444330d63
Sam Dension <sdenison@satx.rr.com>
2023-10-30 20:04:23 -0500

    SD - dynamo#23 - Basic grid is working.

---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Navigation/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Dynamo.Business.Shared.AdventOfCode.Fuel;
+
+namespace Dynamo.Business.Shared.AdventOfCode.Navigation
+{
+    public class Grid
+    {
+        public List<Coordinate> Coordinates { get; } = new List<Coordinate>();
+        public GridPoint[,] GridPoints { get; }
+
+
+
+        public Grid(string[] coordinates)
+        {
+            for (var i = 0; i < coordinates.Length; i++)
+                Coordinates.Add(new Coordinate(coordinates[i], ((char)('A' + i)).ToString()));
+
+            var maxXSize = Coordinates.Max(x => x.X) + 1;
+            var maxYSize = Coordinates.Max(x => x.Y) + 1;
+            var maxSize = maxXSize > maxYSize ? maxXSize : maxYSize;
+            //Adding 1 since everything is zero based.
+            GridPoints = new GridPoint[maxSize, maxSize];
+            for (var x = 0; x < maxSize; x++)
+                for (var y = 0; y < maxSize; y++)
+                {
+                    var minManhattan = Coordinates.Min(_ => _.GetManhattanDistance(x, y));
+                    if (Coordinates.Count(_ => _.GetManhattanDistance(x, y) == minManhattan) == 1)
+                    {
+                        var ownedBy = Coordinates.Single(_ => _.GetManhattanDistance(x, y) == minManhattan);
+                        GridPoints[x, y] = new GridPoint(ownedBy);
+                    }
+                    else
+                    {
+                        GridPoints[x, y] = new GridPoint();
+                    }
+                }
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/Navigation/GridTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Business.Shared.AdventOfCode.Navigation;
+using NUnit.Framework;
+
+namespace Dynamo.Business.Unit.Tests.AdventOfCode.Navigation
+{
+    [TestFixture]
+    public class GridTests
+    {
+        [Test]
+        public void Can_create_a_grid()
+        {
+            var testData = GetTestData();
+            var grid = new Grid(testData);
+
+            Assert.AreEqual("A", grid.GridPoints[0, 0].OwnedBy.Name);
+            Assert.AreEqual("A", grid.GridPoints[1, 1].OwnedBy.Name);
+            Assert.IsNull(grid.GridPoints[5, 0].OwnedBy);
+            Assert.AreEqual("C", grid.GridPoints[6, 0].OwnedBy.Name);
+            Assert.AreEqual("E", grid.GridPoints[5, 2].OwnedBy.Name);
+            Assert.AreEqual("F", grid.GridPoints[8, 9].OwnedBy.Name);
+            Assert.AreEqual("F", grid.GridPoints[9, 9].OwnedBy.Name);
+
+        }
+
+        private static string[] GetTestData()
+        {
+            return new string[]
+            {
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9"
+            };
+        }
+    }
+}
