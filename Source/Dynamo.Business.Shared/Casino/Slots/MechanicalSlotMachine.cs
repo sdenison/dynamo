193b4096167c38e7f4295d5113dabeea707a8e75
Sam Dension <sdenison@satx.rr.com>
2024-04-18 18:36:39 -0500

    SD - dynamo#31 - Create the bones of the mechanical slot machine.

---
+++ Source/Dynamo.Business.Shared/Casino/Slots/MechanicalSlotMachine.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Business.Shared.Casino.Slots
+{
+    public class MechanicalSlotMachine
+    {
+    }
+
+    public class Reel
+    {
+        public ReelSymbol CurrentSymbol { get; private set; }
+
+        public void Next()
+        {
+            CurrentSymbol = CurrentSymbol.NextSymbol;
+        }
+
+        public void Next(int numberToMove)
+        {
+            for (var i = 0; i < numberToMove; i++)
+            {
+                CurrentSymbol = CurrentSymbol.NextSymbol;
+            }
+        }
+
+        public Reel(string symbolsString) : this(ConvertStringToSymbols(symbolsString))
+        {
+        }
+
+        public Reel(List<Symbol> symbols)
+        {
+            var firstSymbol = new ReelSymbol()
+            {
+                Symbol = symbols[0]
+            };
+            CurrentSymbol = firstSymbol;
+
+            foreach (var currentSymbol in symbols.Skip(1))
+            {
+                var reelSymbol = new ReelSymbol()
+                {
+                    Symbol = currentSymbol,
+                    PreviousSymbol = CurrentSymbol
+                };
+                CurrentSymbol.NextSymbol = reelSymbol;
+                CurrentSymbol = reelSymbol;
+            }
+            firstSymbol.PreviousSymbol = CurrentSymbol;
+            CurrentSymbol = firstSymbol;
+        }
+
+        private static List<Symbol> ConvertStringToSymbols(string symbolsString)
+        {
+            List<string> symbolNames = symbolsString.Split(',').Select(s => s.Trim()).ToList();
+            List<Symbol> symbolsAsEnum = symbolNames
+                .Select(s => s.Replace(" ", "").Replace("13", "Thirteen"))
+                .Select(s =>
+                {
+                    Symbol result;
+                    if (Enum.TryParse(s, true, out result))
+                        return result;
+                    else
+                        throw new ArgumentException($"Invalid symbol name: {s}");
+                })
+                .ToList();
+
+            return symbolsAsEnum;
+        }
+    }
+
+    public class ReelSymbol
+    {
+        public Symbol Symbol { get; set; }
+        public ReelSymbol NextSymbol { get; set; }
+        public ReelSymbol PreviousSymbol { get; set; }
+    }
+
+    public enum Symbol
+    {
+        Guitar,
+        HeartHands,
+        Thirteen,
+        RedLips,
+        RedScarf,
+        StatueOfLiberty,
+        Snake,
+        Butterfly,
+        Cardigan,
+        Champagne,
+        FriendshipBracelet,
+        QuillAndInk
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/Casino/Slots/MechanicalSlotMachineTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Business.Shared.Casino.Slots;
+using NUnit.Framework;
+
+namespace Dynamo.Business.Unit.Tests.Casino.Slots
+{
+    public class MechanicalSlotMachineTests
+    {
+
+        [Test]
+        public void Can_create_a_reel()
+        {
+            var reelString = ReelStrings()[0];
+            var reel = new Reel(reelString);
+            Assert.That(reel.CurrentSymbol.Symbol, Is.EqualTo(Symbol.Butterfly));
+            Assert.That(reel.CurrentSymbol.NextSymbol.Symbol, Is.EqualTo(Symbol.RedScarf));
+            Assert.That(reel.CurrentSymbol.PreviousSymbol.Symbol, Is.EqualTo(Symbol.QuillAndInk));
+
+            //Move to the next symbol
+            reel.Next();
+            Assert.That(reel.CurrentSymbol.Symbol, Is.EqualTo(Symbol.RedScarf));
+            Assert.That(reel.CurrentSymbol.NextSymbol.Symbol, Is.EqualTo(Symbol.Guitar));
+            Assert.That(reel.CurrentSymbol.PreviousSymbol.Symbol, Is.EqualTo(Symbol.Butterfly));
+        }
+
+        public List<string> ReelStrings()
+        {
+            return new List<string>
+            {
+                "Butterfly, Red Scarf, Guitar, Red Scarf, Cardigan, Snake, Butterfly, Heart hands, Butterfly, Red Lips, 13, Red Lips, 13, Cardigan, Friendship Bracelet, Red Lips, Red Scarf, Heart hands, Snake, Quill and Ink",
+                "Heart hands, Snake, Champagne, Cardigan, Quill and Ink, Cardigan, Cardigan, Snake, Red Scarf, Snake, Champagne, Friendship Bracelet, Snake, Butterfly, Red Scarf, Butterfly, 13, Snake, Cardigan, Champagne",
+                "Snake, 13, Friendship Bracelet, Red Scarf, Guitar, Red Scarf, Cardigan, 13, Guitar, Red Lips, Red Lips, Red Lips, Quill and Ink, Cardigan, Champagne, Red Scarf, Cardigan, Red Scarf, Statue of Liberty, Heart hands"
+            };
+        }
+
+    }
+}
