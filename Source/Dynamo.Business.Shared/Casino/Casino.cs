c61b9ecc30e3ad20e7b3ec6e0c422fcec1381667
Sam Dension <sdenison@satx.rr.com>
2024-04-24 22:58:56 -0500

    SD - dynamo#32 - Got week 3 part 1 answer, but it's ugly.

---
+++ Source/Dynamo.Business.Shared/Casino/Casino.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Business.Shared.Casino
+{
+    public class Casino
+    {
+        public SortedDictionary<double, Player> Players { get; set; }
+        public double CurrentTime { get; set; }
+        public double TotalTime { get; set; }
+
+        public Casino()
+        {
+            Players = new SortedDictionary<double, Player>();
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (Players.Count >= 1000)
+            {
+                var nextPlayerToExit = Players.FirstOrDefault();
+                TotalTime = CurrentTime + nextPlayerToExit.Key;
+                Players.Remove(nextPlayerToExit.Key);
+            }
+            var exitTime = player.TimeInCasino + CurrentTime;
+            Players.Add(exitTime, player);
+            CurrentTime += 0.0021739130;
+
+            var playersToRemove = Players.Where(x => x.Key < CurrentTime).ToList();
+            foreach (var playerToRemove in playersToRemove)
+            {
+                Players.Remove(playerToRemove.Key);
+            }
+
+        }
+    }
+
+    public class Player
+    {
+        public double TimeInCasino { get; set; }
+
+    }
+
+    public abstract class Game
+    {
+
+    }
+
+    public class Blackjack : Game
+    {
+    }
+
+    public static class MathHelper
+    {
+        public static double[] GenerateExponentialRandomVariables(double scale, int size)
+        {
+            var random = new Random();
+            double[] values = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                values[i] = -scale * Math.Log(1 - random.NextDouble());
+            }
+            return values;
+        }
+    }
+
+}
---
+++ Source/Dynamo.Business.Unit.Tests/Casino/CasinoTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Business.Shared.Casino;
+using NUnit.Framework;
+
+namespace Dynamo.Business.Unit.Tests.Casino
+{
+    public class CasinoTests
+    {
+        [Test]
+        public void Can_create_a_casino()
+        {
+            var casino = new Shared.Casino.Casino();
+            int n = 11040; //This is 460 per hour for 24 hours
+            double scale = 2; //This is the average of how long they spend in the casino
+
+            double[] timeInCasino = MathHelper.GenerateExponentialRandomVariables(scale, n);
+
+
+
+            for (var i = 0; i < timeInCasino.Length; i++)
+            {
+                var x = timeInCasino[i];
+            }
+
+            casino.TotalTime = 24; //24 hours in seconds
+
+            var numbersOfUsers = new List<int>();
+
+            var playerNum = 0;
+            while (casino.CurrentTime < casino.TotalTime && playerNum < timeInCasino.Length)
+            {
+                var player = new Player() { TimeInCasino = timeInCasino[playerNum] };
+                casino.AddPlayer(player);
+                playerNum++;
+
+                if (casino.CurrentTime > 12)
+                {
+                    var numberOfUsers = casino.Players.Count;
+                    numbersOfUsers.Add(numberOfUsers);
+                }
+            }
+
+            var averageNumberOfUsers = numbersOfUsers.Sum(x => x) / numbersOfUsers.Count;
+            //Accepted answer was 927
+            Assert.That(averageNumberOfUsers, Is.GreaterThan(908));
+            Assert.That(averageNumberOfUsers, Is.LessThan(934));
+
+            //Assert.That(timeInCasino.Length, Is.EqualTo(0));
+
+        }
+    }
+}
