9c532a685abc9fb7d0e582d97f71323c16ce2019
Sam Dension <sdenison@satx.rr.com>
2024-04-29 19:03:06 -0500

    SD - dynamo#33 - Creating a player to play stree dice.

---
+++ Source/Dynamo.Business.Shared/Casino/StreetDice/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Business.Shared.Casino.StreetDice
+{
+    public class Player
+    {
+        public int PlayerId { get; private set; }
+        public int CurrentMoney { get; set; }
+        public double MaximumPercentage { get; private set; }
+        public List<Player> Nemeses { get; set; }
+        public List<int> NemesisIds { get; private set; }
+
+        public static Player Parse(string playerString)
+        {
+            var playerId = int.Parse(playerString.Substring(0, playerString.IndexOf(':')));
+            var currentMoney = int.Parse(playerString.Split(' ')[1].Replace("$", ""));
+            var percentInt = int.Parse(playerString.Split(' ')[2].Replace("%", " "));
+            var maximumPercentage = (double)int.Parse(playerString.Split(' ')[2]) / 100;
+            var nemesisIds = playerString.Split(" ")[3].Split(",").Select(x => int.Parse(x)).ToList();
+
+            return new Player(playerId, currentMoney, maximumPercentage, nemesisIds);
+        }
+
+        public Player(int playerId, int currentMoney, double maximumPercentage, List<int> nemesisIds)
+        {
+            PlayerId = playerId;
+            CurrentMoney = currentMoney;
+            MaximumPercentage = maximumPercentage;
+            NemesisIds = nemesisIds;
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/Casino/StreetDice/PlayerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Frameworks;
+using NUnit.Framework;
+using Dynamo.Business.Shared.Casino.StreetDice;
+
+namespace Dynamo.Business.Unit.Tests.Casino.StreetDice
+{
+    public class PlayerTests
+    {
+        [Test]
+        public void Can_parse_player_string()
+        {
+            var playerString = "1: $40 50% -4,3";
+            var player = Player.Parse(playerString);
+            Assert.That(player.PlayerId, Is.EqualTo(1));
+            Assert.That(player.CurrentMoney, Is.EqualTo(40));
+            Assert.That(player.MaximumPercentage, Is.EqualTo(0.50));
+            Assert.That(player.NemesisIds, Is.Not.Empty);
+            Assert.That(player.NemesisIds.Count, Is.EqualTo(2));
+            Assert.That(player.NemesisIds[0], Is.EqualTo(-4));
+            Assert.That(player.NemesisIds[1], Is.EqualTo(3));
+        }
+    }
+}
