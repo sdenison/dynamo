57b97996c37558d3ed9cc400e26e9e7d7a763e05
Sam Dension <sdenison@satx.rr.com>
2024-06-08 16:35:27 -0500

    SD - dynamo#36 - More playing with Blazor.

---
+++ Source/Dynamo.Ui.Web/Data/IdentityDynamoUiWebContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Dynamo.Ui.Web.Data
+{
+    public class IdentityDynamoUiWebContext(DbContextOptions<IdentityDynamoUiWebContext> options) : IdentityDbContext<IdentityUser>(options)
+    {
+    }
+}
