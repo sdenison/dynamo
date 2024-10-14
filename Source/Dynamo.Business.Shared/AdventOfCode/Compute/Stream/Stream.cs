903b6d1579e0981b0746ffe5af52fe5cf4d1a10b
Sam Dension <sdenison@satx.rr.com>
2024-10-14 17:33:37 -0500

    SD - dynamo#38 - Solving day 9 2017 puzzle.

---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/Stream/Stream.cs
@@ -0,0 +1,26 @@
+﻿namespace Dynamo.Business.Shared.AdventOfCode.Compute.Stream
+{
+    public class Stream
+    {
+        public char[] StreamChars { get; private set; }
+        public int Index { get; set; }
+
+        public Stream(string stream)
+        {
+            StreamChars = stream.ToCharArray();
+            Index = 0;
+        }
+
+        public bool HasNextValue()
+        {
+            return Index < StreamChars.Length;
+        }
+
+        public char GetNextChar()
+        {
+            var nextChar = StreamChars[Index];
+            Index++;
+            return nextChar;
+        }
+    }
+}
