2a9e2e73f94fecb88c9ba8299dd5589aa515795b
Denison <sdenison@lmi.org>
2024-10-25 16:51:24 -0500

    SD - dynamo#39 - Can get 2017 day 15 part 1.

--- Source/Dynamo.Business.Shared/AdventOfCode/Compute/DuelingGenerators/Judge.cs
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/DuelingGenerators/Judge.cs
@@ -12,5 +12,20 @@ namespace Dynamo.Business.Shared.AdventOfCode.Compute.DuelingGenerators
         {
             _generators = generators;
         }
+
+        public int CountMatches(int pairsToProcess)
+        {
+            var matches = 0;
+            for(var i = 0; i < pairsToProcess; i++) 
+            {
+                var generator1Value = _generators[0].NextNumber();
+                var generator2Value = _generators[1].NextNumber();
+                if ((generator1Value % 65536) ==  (generator2Value % 65536))
+                {
+                    matches++;
+                }
+            }
+            return matches;
+        }
     }
 }
--- Source/Dynamo.Business.Unit.Tests/AdventOfCode/DuelingGenerators/GeneratorTests.cs
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/DuelingGenerators/GeneratorTests.cs
@@ -35,5 +35,35 @@ namespace Dynamo.Business.Unit.Tests.AdventOfCode.DuelingGenerators
             nextNumber = generatorB.NextNumber();
             Assert.That(nextNumber, Is.EqualTo(285222916));
         }
+
+        [Test]
+        public void Judge_can_return_number_of_matches()
+        {
+            var generatorA = new Generator(id: "A", 16807, 65);
+            var generatorB = new Generator(id: "B", 48271, 8921);
+            var judge = new Judge(new List<Generator> { generatorA, generatorB });
+            var matches = judge.CountMatches(5);
+            Assert.That(matches, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Judge_can_return_number_of_matches_forty_million()
+        {
+            var generatorA = new Generator(id: "A", 16807, 65);
+            var generatorB = new Generator(id: "B", 48271, 8921);
+            var judge = new Judge(new List<Generator> { generatorA, generatorB });
+            var matches = judge.CountMatches(40000000);
+            Assert.That(matches, Is.EqualTo(588));
+        }
+
+        [Test]
+        public void Judge_can_get_2017_day_15_part_1()
+        { 
+            var generatorA = new Generator(id: "A", 16807, 512);
+            var generatorB = new Generator(id: "B", 48271, 191);
+            var judge = new Judge(new List<Generator> { generatorA, generatorB });
+            var matches = judge.CountMatches(40000000);
+            Assert.That(matches, Is.EqualTo(567));
+        }
     }
 }
