4d7788abab8e23c8fc7c7f29126478da4e3c87dd
Denison <sdenison@lmi.org>
2024-10-26 15:21:55 -0500

    SD - dynamo#39 - Refactoring the Judge so that generators can be added one my one instead of the list of generators.

--- Source/Dynamo.Business.Shared/AdventOfCode/Compute/DuelingGenerators/Judge.cs
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/DuelingGenerators/Judge.cs
@@ -1,28 +1,22 @@
-﻿using System.Collections.Generic;
-
-namespace Dynamo.Business.Shared.AdventOfCode.Compute.DuelingGenerators
+﻿namespace Dynamo.Business.Shared.AdventOfCode.Compute.DuelingGenerators
 {
     public class Judge
     {
-        private readonly List<IGenerator> _generators;
+        private readonly IGenerator _generatorA;
+        private readonly IGenerator _generatorB;
 
-        public Judge(List<IGenerator> generators)
+        public Judge(IGenerator generatorA, IGenerator generatorB)
         {
-            _generators = generators;
+            _generatorA = generatorA;
+            _generatorB = generatorB;
         }
 
         public int CountMatches(int pairsToProcess)
         {
             var matches = 0;
             for(var i = 0; i < pairsToProcess; i++) 
-            {
-                var generator1Value = _generators[0].NextNumber();
-                var generator2Value = _generators[1].NextNumber();
-                if ((generator1Value % 65536) ==  (generator2Value % 65536))
-                {
+                if ((_generatorA.NextNumber() % 65536) == (_generatorB.NextNumber() % 65536))
                     matches++;
-                }
-            }
             return matches;
         }
     }
--- Source/Dynamo.Business.Unit.Tests/AdventOfCode/DuelingGenerators/GeneratorTests.cs
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/DuelingGenerators/GeneratorTests.cs
@@ -1,11 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dynamo.Business.Shared.AdventOfCode.Compute.DuelingGenerators;
-
 
 namespace Dynamo.Business.Unit.Tests.AdventOfCode.DuelingGenerators
 {
@@ -41,29 +35,27 @@ namespace Dynamo.Business.Unit.Tests.AdventOfCode.DuelingGenerators
         {
             var generatorA = new Generator(id: "A", 16807, 65);
             var generatorB = new Generator(id: "B", 48271, 8921);
-            var judge = new Judge(new List<IGenerator> { generatorA, generatorB });
+            var judge = new Judge(generatorA, generatorB);
             var matches = judge.CountMatches(5);
             Assert.That(matches, Is.EqualTo(1));
         }
 
         [Test, Ignore("Takes too long")]
-        //[Test]
         public void Judge_can_return_number_of_matches_forty_million()
         {
             var generatorA = new Generator(id: "A", 16807, 65);
             var generatorB = new Generator(id: "B", 48271, 8921);
-            var judge = new Judge(new List<IGenerator> { generatorA, generatorB });
+            var judge = new Judge(generatorA, generatorB);
             var matches = judge.CountMatches(40000000);
             Assert.That(matches, Is.EqualTo(588));
         }
 
         [Test, Ignore("Takes too long")]
-        //[Test]
         public void Judge_can_get_2017_day_15_part_1()
         { 
             var generatorA = new Generator(id: "A", 16807, 512);
             var generatorB = new Generator(id: "B", 48271, 191);
-            var judge = new Judge(new List<IGenerator> { generatorA, generatorB });
+            var judge = new Judge(generatorA, generatorB);
             var matches = judge.CountMatches(40000000);
             Assert.That(matches, Is.EqualTo(567));
         }
@@ -73,18 +65,17 @@ namespace Dynamo.Business.Unit.Tests.AdventOfCode.DuelingGenerators
         {
             var generatorA = new MultipleGenerator(id: "A", 16807, 65, 4);
             var generatorB = new MultipleGenerator(id: "B", 48271, 8921, 8);
-            var judge = new Judge(new List<IGenerator> { generatorA, generatorB });
+            var judge = new Judge(generatorA, generatorB);
             var matches = judge.CountMatches(1056);
             Assert.That(matches, Is.EqualTo(1));
         }
 
-        //[Test, Ignore("Takes too long")]
-        [Test]
+        [Test, Ignore("Takes too long")]
         public void Judge_can_solve_2017_day_15_part_2()
         {
             var generatorA = new MultipleGenerator(id: "A", 16807, 512, 4);
             var generatorB = new MultipleGenerator(id: "B", 48271, 191, 8);
-            var judge = new Judge(new List<IGenerator> { generatorA, generatorB });
+            var judge = new Judge(generatorA, generatorB);
             var matches = judge.CountMatches(5000000);
             Assert.That(matches, Is.EqualTo(323));
         }
