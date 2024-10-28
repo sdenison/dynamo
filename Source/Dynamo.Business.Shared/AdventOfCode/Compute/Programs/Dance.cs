8204790a25998366486bc814d61e3237308ed221
Denison <sdenison@lmi.org>
2024-10-27 19:06:33 -0500

    SD - dynamo#39 - Starting on the dance problem.

---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/Programs/Dance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamo.Business.Shared.AdventOfCode.Compute.Programs
+{
+    public class Dance
+    {
+        public readonly char[] _programs;
+
+        public Dance(char[] programs)
+        {
+            _programs = programs;
+        }
+
+        public char[] DoDance(string allMoves)
+        {
+
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/Programs/DanceMove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamo.Business.Shared.AdventOfCode.Compute.Programs
+{
+    public class DanceMove
+    {
+        public Operation Operation { get; set; }
+        public int? Spin { get; set; }
+        public int? Program1Index { get; set; }
+        public int? Program2Index { get; set; }
+        public string Program1Name { get; set; }
+        public string Program2Name { get; set; }
+
+        public DanceMove(string danceMove)
+        {
+            if (danceMove[0] == 's')
+            {
+                Operation = Operation.Spin;
+                danceMove = danceMove.Substring(1);
+                Spin = int.Parse(danceMove);
+            }
+            if (danceMove[0] == 'x')
+            {
+                Operation = Operation.SwapByPositions;
+                danceMove = danceMove.Substring(1);
+                Program1Index = int.Parse(danceMove.Split('/')[0]);
+                Program2Index = int.Parse(danceMove.Split('/')[1]);
+            }
+            if (danceMove[0] == 'p')
+            {
+                Operation = Operation.SwapByProgramName;
+                danceMove = danceMove.Substring(1);
+                Program1Name = danceMove.Split('/')[0];
+                Program2Name = danceMove.Split('/')[1];
+            }
+        }
+    }
+}
