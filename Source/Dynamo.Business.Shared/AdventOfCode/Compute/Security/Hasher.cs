f98bdbb4a5a484200dd257dadc1cfdddbdccbb29
Sam Dension <sdenison@satx.rr.com>
2024-10-28 20:48:12 -0500

    SD - dynamo#39 - Basics of the hasher is done.

---
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/Security/Hasher.cs
@@ -0,0 +1,37 @@
+﻿namespace Dynamo.Business.Shared.AdventOfCode.Compute.Security
+{
+    public class Hasher
+    {
+        public int[] Elements { get; private set; }
+        public int CurrentPosition { get; private set; }
+        public int SkipSize { get; private set; }
+
+        public Hasher(int size)
+        {
+            CurrentPosition = 0;
+            SkipSize = 0;
+            Elements = new int[size];
+            for (var i = 0; i < size; i++)
+            {
+                Elements[i] = i;
+            }
+        }
+
+        public int GetElement(int index)
+        {
+            return Elements[index % Elements.Length];
+        }
+
+        public void ApplyLength(int length)
+        {
+            for (var i = 0; i < length / 2; i++)
+            {
+                var element1 = Elements[i + CurrentPosition];
+                var element2 = Elements[CurrentPosition + ((length - 1) - i)];
+                Elements[i + CurrentPosition] = element2;
+                Elements[CurrentPosition + ((length - 1) - i)] = element1;
+            }
+            CurrentPosition += SkipSize + length;
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/Compute/Security/HasherTests.cs
@@ -0,0 +1,27 @@
+﻿using Dynamo.Business.Shared.AdventOfCode.Compute.Security;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Dynamo.Business.Unit.Tests.AdventOfCode.Compute.Security
+{
+    [TestFixture]
+    public class HasherTests
+    {
+        [Test]
+        public void Can_apply_lengths_Hasher()
+        {
+            var hasher = new Hasher(5);
+            Assert.That(hasher.Elements.Count, Is.EqualTo(5));
+            Assert.That(hasher.GetElement(0), Is.EqualTo(0));
+            Assert.That(hasher.GetElement(4), Is.EqualTo(4));
+            Assert.That(hasher.GetElement(5), Is.EqualTo(0));
+            Assert.That(hasher.GetElement(6), Is.EqualTo(1));
+            Assert.That(hasher.GetElement(10), Is.EqualTo(0));
+            hasher.ApplyLength(3);
+            Assert.That(hasher.GetElement(0), Is.EqualTo(2));
+            Assert.That(hasher.GetElement(1), Is.EqualTo(1));
+            Assert.That(hasher.GetElement(2), Is.EqualTo(0));
+            Assert.That(hasher.CurrentPosition, Is.EqualTo(3));
+        }
+    }
+}
