ce3202aada3776aaba3e2ae5830b50891748e846
Sam Dension <sdenison@satx.rr.com>
2024-11-07 17:55:56 -0600

    SD - dynamo#40 - Can get day 7 example.

--- Source/Dynamo.Business.Shared/AdventOfCode/Compute/Programs/Program.cs
+++ Source/Dynamo.Business.Shared/AdventOfCode/Compute/Programs/Program.cs
@@ -1,12 +1,64 @@
-﻿namespace Dynamo.Business.Shared.AdventOfCode.Compute.Programs
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dynamo.Business.Shared.AdventOfCode.Compute.Programs
 {
     public class Program
     {
         public string Name { get; private set; }
+        public int Weight { get; private set; }
+        public List<Program> Children { get; private set; }
+        public bool HasParent { get; private set; }
 
-        public Program(string name)
+        public Program(string name, int weight)
         {
             Name = name;
-        }   
+            Weight = weight;
+            Children = new List<Program>();
+            HasParent = false;
+        }
+
+        public static Program Parse(string programString)
+        {
+            var name = programString.Split(' ')[0];
+            var weight = programString.Split(" ")[1].Replace("(", "").Replace(")", "");
+            return new Program(name, int.Parse(weight));
+        }
+
+        public static Program Parse(List<string> programStrings)
+        {
+            var programs = new Dictionary<string, Program>();
+            // Load all programs
+            foreach (var programString in programStrings)
+            {
+                var program = Program.Parse(programString);
+                programs.Add(program.Name, program);
+            }
+            // Handle programs with children
+            foreach (var programString in programStrings.Where(x => x.Contains("->")))
+            {
+                var programName = programString.Split(" ")[0].Trim();
+                foreach (var childProgramName in programString.Split("->")[1].Split(","))
+                {
+                    programs[programName].AddChild(programs[childProgramName.Trim()]);
+                }
+            }
+            // Return the program with no parents
+            return programs.Values.Single(x => !x.HasParent);
+        }
+
+        public void AddChild(Program child)
+        {
+            child.HasParent = true;
+            Children.Add(child);
+        }
+
+        public Program GetLastChild()
+        {
+            if (Children.Any())
+                return Children[Children.Count - 1].GetLastChild();
+            else
+                return this;
+        }
     }
 }
---
+++ Source/Dynamo.Business.Unit.Tests/AdventOfCode/Compute/Programs/ProgramTests.cs
@@ -0,0 +1,43 @@
+﻿using Dynamo.Business.Shared.AdventOfCode.Compute.Programs;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Dynamo.Business.Unit.Tests.AdventOfCode.Compute.Programs
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Can_create_Program()
+        {
+            var program = Program.Parse("pbga (66)");
+            Assert.That(program.Name, Is.EqualTo("pbga"));
+            Assert.That(program.Weight, Is.EqualTo(66));
+        }
+
+        [Test]
+        public void Can_create_Program_data_structure_from_list_of_string()
+        {
+            var programStrings = new List<string>()
+            {
+                "pbga (66)",
+                "xhth (57)",
+                "ebii (61)",
+                "havc (66)",
+                "ktlj (57)",
+                "fwft (72) -> ktlj, cntj, xhth",
+                "qoyq (66)",
+                "padx (45) -> pbga, havc, qoyq",
+                "tknk (41) -> ugml, padx, fwft",
+                "jptl (61)",
+                "ugml (68) -> gyxo, ebii, jptl",
+                "gyxo (61)",
+                "cntj (57)"
+            };
+            var program = Program.Parse(programStrings);
+            Assert.That(program.Name, Is.EqualTo("tknk"));
+            var lastChild = program.GetLastChild();
+            Assert.That(lastChild.Name, Is.EqualTo("xhth"));
+        }
+    }
+}
