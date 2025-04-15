9cf70ec1375730d29db3cb1cefd55d720f42cff7
Sam Dension <sdenison@satx.rr.com>
2025-04-14 20:57:53 -0500

    SD - dynamo#42 - Consolidating the shifts.

--- Source/Dynamo.Business.Shared/Cyber/CharacterShifter.cs
+++ Source/Dynamo.Business.Shared/Cyber/CharacterShifter.cs
@@ -5,24 +5,18 @@
         public static char Shift(char c, int shift)
         {
             if (c >= 'a' && c <= 'z')
-                return ShiftLower(c, shift);
+                return Shift(c, shift, 'a');
 
             if (c >= 'A' && c <= 'Z')
-                return ShiftUpper(c, shift);
+                return Shift(c, shift, 'A');
 
             return c;
         }
 
-        private static char ShiftLower(char c, int shift)
+        private static char Shift(char c, int shift, char beginnningChar)
         {
-            var index = (int)c - 97;
-            return (char)(((index + shift) % 26) + 97);
-        }
-
-        private static char ShiftUpper(char c, int shift)
-        {
-            var index = (int)c - 65;
-            return (char)(((index + shift) % 26) + 65);
+            var index = c - beginnningChar;
+            return (char)(((index + shift) % 26) + beginnningChar);
         }
     }
 }
