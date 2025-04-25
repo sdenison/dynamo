6b868b6216951f3bf14f57f697cc44c7bf785b3e
Sam Dension <sdenison@satx.rr.com>
2025-04-24 20:58:10 -0500

    SD - dynamo#43 - Scanning the files for the message.

--- Source/Dynamo.Business.Shared/Cyber/Scanner/FolderScanner.cs
+++ Source/Dynamo.Business.Shared/Cyber/Scanner/FolderScanner.cs
@@ -1,11 +1,40 @@
-﻿namespace Dynamo.Business.Shared.Cyber.Scanner
+﻿using System.IO;
+
+namespace Dynamo.Business.Shared.Cyber.Scanner
 {
     public class FolderScanner
     {
         public static string ScanFolder(string folder, string start, string end)
         {
+            //return string.Empty;
+            //var message
+            var dirInfo = new DirectoryInfo(folder);
+            foreach (var file in dirInfo.GetFiles())
+            {
+                using (var stream = file.OpenRead())
+                using (var reader = new StreamReader(stream))
+                {
+                    string fileContents = reader.ReadToEnd();
+                    if (fileContents.Contains(start) && fileContents.Contains(end))
+                    {
+                        return FindMessage(fileContents, start, end);
+                    }
+                }
+            }
             return string.Empty;
         }
+
+        public static int CountTheChar(string strToAnalyze, char charToCount)
+        {
+            var count = 0;
+            foreach (var currentChar in strToAnalyze.ToCharArray())
+            {
+                if (currentChar == charToCount)
+                    count++;
+            }
+            return count;
+        }
+
 
         public static string FindMessage(string textToSearch, string start, string end)
         {
--- Source/Dynamo.Business.Unit.Tests/Cyber/ScannerTests.cs
+++ Source/Dynamo.Business.Unit.Tests/Cyber/ScannerTests.cs
@@ -8,7 +8,6 @@ namespace Dynamo.Business.Unit.Tests.Cyber
         [Test]
         public void Can_find_text_between_start_and_end()
         {
-            var folder = "D:\\temp\\Problem2SystemFiles";
             var prefix = "alksdjfjfeklwjfklefj;alwkefjklaejf";
             var start = "4920414d2054574f20464f4f4c53";
             var middle = "aabbbbbweffkefjaaa"; // Has 5 a's
@@ -17,6 +16,17 @@ namespace Dynamo.Business.Unit.Tests.Cyber
             var wholeText = prefix + start + middle + end + suffix;
             var message = FolderScanner.FindMessage(wholeText, start, end);
             Assert.That(message, Is.EqualTo(middle));
+            Assert.That(FolderScanner.CountTheChar(message, 'a'), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Can_count_number_of_char_a_in_files()
+        {
+            var folder = "D:\\temp\\Problem2SystemFiles";
+            var start = "4920414d2054574f20464f4f4c53";
+            var end = "444945204e4f542c20504f4f52204445415448";
+            var message = FolderScanner.ScanFolder(folder, start, end);
+            Assert.That(FolderScanner.CountTheChar(message, 'a'), Is.EqualTo(820));
         }
     }
 }
