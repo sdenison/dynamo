b3a947a2408af2a9a62ab10e1090ee91cba9c37c
Sam Dension <sdenison@satx.rr.com>
2025-05-03 18:15:54 -0500

    SD - dynamo#44 - Working on the log analyzer.

--- Source/Dynamo.Business.Shared/Cyber/LogAnalyzer/LogAnalyzer.cs
+++ Source/Dynamo.Business.Shared/Cyber/LogAnalyzer/LogAnalyzer.cs
@@ -1,7 +1,45 @@
-﻿namespace Dynamo.Business.Shared.Cyber.LogAnalyzer
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dynamo.Business.Shared.Cyber.LogAnalyzer
 {
     public class LogAnalyzer
     {
-        //public LogAnalyzer(string logs)
+        private List<LogEntry> LogEntries { get; set; }
+
+        public LogAnalyzer(List<LogEntry> logEntries)
+        {
+            LogEntries = logEntries.OrderBy(x => x.TimeStampString).ToList();
+        }
+
+        public List<string> FindUnauthorizedRequests()
+        {
+            return LogEntries.Where(x =>
+                x.Endpoint.StartsWith("/admin/config") ||
+                x.Endpoint.StartsWith("/admin/users") ||
+                x.Endpoint.StartsWith("/sensitive")
+                ).Select(x => x.IpAddress).Distinct().ToList();
+        }
+
+        public List<string> FindFailedLogins()
+        {
+            var failedLogins = new Dictionary<string, int>();
+            foreach (var logEntry in LogEntries.Where(x => x.StatusCode == 401 || x.StatusCode == 403))
+            {
+                if (!failedLogins.ContainsKey(logEntry.IpAddress))
+                    failedLogins.Add(logEntry.IpAddress, 1);
+                else
+                    failedLogins[logEntry.IpAddress] += 1;
+            }
+            var ipList = new List<string>();
+            foreach (var ip in failedLogins.Keys)
+            {
+                if (failedLogins[ip] >= 5)
+                {
+                    ipList.Add(ip);
+                }
+            }
+            return ipList;
+        }
     }
 }
--- Source/Dynamo.Business.Unit.Tests/Cyber/LogAnalyzerTests.cs
+++ Source/Dynamo.Business.Unit.Tests/Cyber/LogAnalyzerTests.cs
@@ -22,5 +22,25 @@ namespace Dynamo.Business.Unit.Tests.Cyber
             var earliestLoginFailed = logEntries.First(x => x.StatusCode == 401 || x.StatusCode == 403);
             Assert.That(earliestLoginFailed.TimeStampString, Is.EqualTo("2023-09-15T00:33:07.207000Z"));
         }
+
+        [Test]
+        public void Can_get_unauthorized_requests()
+        {
+            var stream = FileGetter.GetMemoryStreamFromFile("server_logs.json");
+            var logEntries = JsonSerializer.Deserialize<List<LogEntry>>(stream);
+            var logAnalyzer = new LogAnalyzer(logEntries);
+            var unauthorized = logAnalyzer.FindUnauthorizedRequests();
+            Assert.That(unauthorized.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Can_get_repeated_failed_logins()
+        {
+            var stream = FileGetter.GetMemoryStreamFromFile("server_logs.json");
+            var logEntries = JsonSerializer.Deserialize<List<LogEntry>>(stream);
+            var logAnalyzer = new LogAnalyzer(logEntries);
+            var failedLogins = logAnalyzer.FindFailedLogins();
+            Assert.That(failedLogins.Count, Is.EqualTo(3));
+        }
     }
 }
