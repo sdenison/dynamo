30a8fb5c1a53351c09dfd40e07a33d45818f0824
Sam Dension <sdenison@satx.rr.com>
2025-05-10 17:35:31 -0500

    SD - dynamo#45 - Making things work with lists a bit more naturally.

--- Source/Dynamo.Business.Shared/Cyber/Passwords/PasswordAnalyzer.cs
+++ Source/Dynamo.Business.Shared/Cyber/Passwords/PasswordAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Dynamo.Business.Shared.Cyber.Passwords
@@ -10,6 +11,61 @@ namespace Dynamo.Business.Shared.Cyber.Passwords
             var numberOfSymbols = password.ToCharArray().Distinct().Count();
             var entropy = password.Count() * (Math.Log(numberOfSymbols) / Math.Log(2));
             return entropy;
+        }
+
+        public static List<string> ComputeSha1Hash(List<string> inputs)
+        {
+            return inputs.Select(x => ComputeSha1Hash(x)).ToList();
+        }
+
+        public static string ComputeSha1Hash(string input)
+        {
+            using (var sha1 = System.Security.Cryptography.SHA1.Create())
+            {
+                var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                var hashBytes = sha1.ComputeHash(inputBytes);
+                return string.Concat(hashBytes.Select(b => b.ToString("x2")));
+            }
+        }
+
+        public static List<string> ComputeSha256Hash(List<string> inputs)
+        {
+            return inputs.Select(x => ComputeSha256Hash(x)).ToList();
+        }
+
+        public static string ComputeSha256Hash(string input)
+        {
+            using (var sha1 = System.Security.Cryptography.SHA256.Create())
+            {
+                var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                var hashBytes = sha1.ComputeHash(inputBytes);
+                return string.Concat(hashBytes.Select(b => b.ToString("x2")));
+            }
+        }
+
+        public static double GetDifferencePercentage(string input1, string input2)
+        {
+            if (input1.Length != input2.Length)
+                throw new ArgumentException($"Parameters input1 and input2 must be the same length: {input1}   {input2}");
+            var differentChars = 0;
+            for (var i = 0; i < input2.Length; i++)
+            {
+                if (input1[i] != input2[i])
+                    differentChars++;
+            }
+            return ((double)differentChars / input1.Length) * 100;
+        }
+
+        public static double GetDifferencePercentage(List<string> input1List, List<string> input2List)
+        {
+            if (input1List.Count != input2List.Count)
+                throw new ArgumentException($"Parameters input1List and input2List must be the same length");
+            double differencePercentage = 0;
+            for (var i = 0; i < input1List.Count; i++)
+            {
+                differencePercentage += GetDifferencePercentage(input1List[i], input2List[i]);
+            }
+            return (double)differencePercentage / input1List.Count;
         }
     }
 }
--- Source/Dynamo.Business.Unit.Tests/Cyber/PasswordAnalyzerTests.cs
+++ Source/Dynamo.Business.Unit.Tests/Cyber/PasswordAnalyzerTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Dynamo.Business.Unit.Tests.Cyber
 {
@@ -29,6 +30,42 @@ namespace Dynamo.Business.Unit.Tests.Cyber
                     numberOfGoodPasswords++;
             }
             Assert.That(numberOfGoodPasswords, Is.EqualTo(81));
+        }
+
+        [Test]
+        public void Can_get_hash_values_for_examples()
+        {
+            var password = GetPasswordsFromFile("tmyc-week4-pt1-example1.txt").First();
+            var updatedPassword = GetPasswordsFromFile("tmyc-week4-pt1-example2.txt").First();
+
+            var passwordSha1 = PasswordAnalyzer.ComputeSha1Hash(password);
+            Assert.That(passwordSha1, Is.EqualTo("edd7f5c3cbbdac6a1d3aae3b0dbae0e975f56413"));
+            var updatedPasswordSha1 = PasswordAnalyzer.ComputeSha1Hash(updatedPassword);
+            Assert.That(updatedPasswordSha1, Is.EqualTo("6a531ecd01ed447c395a7431dcb65ec82d22eb66"));
+
+            var differencePercentage = PasswordAnalyzer.GetDifferencePercentage(passwordSha1, updatedPasswordSha1);
+            Assert.That(differencePercentage, Is.EqualTo(87.5));
+        }
+
+        [Test]
+        public void Can_get_hash_values_for_example_lists()
+        {
+            var passwords = GetPasswordsFromFile("tmyc-week4-pt1-example1.txt");
+            var updatedPasswords = GetPasswordsFromFile("tmyc-week4-pt1-example2.txt");
+
+            var passwordsSha1 = PasswordAnalyzer.ComputeSha1Hash(passwords);
+            var updatedPasswordsSha1 = PasswordAnalyzer.ComputeSha1Hash(updatedPasswords);
+
+            var differencePercentage = PasswordAnalyzer.GetDifferencePercentage(passwordsSha1, updatedPasswordsSha1);
+            Assert.That(differencePercentage, Is.EqualTo(87.5));
+        }
+
+        [Test]
+        public void Can_get_sha1_list_of_passwords()
+        {
+            var passwords = GetPasswordsFromFile("tmyc-week4-pt1-example1.txt");
+            var hashedPasswords = PasswordAnalyzer.ComputeSha1Hash(passwords);
+            Assert.That(hashedPasswords[0], Is.EqualTo("edd7f5c3cbbdac6a1d3aae3b0dbae0e975f56413"));
         }
 
         public List<string> GetPasswordsFromFile(string fileName)
