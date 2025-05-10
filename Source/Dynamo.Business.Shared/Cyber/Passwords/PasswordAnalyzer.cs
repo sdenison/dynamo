a910c12ede837a89438cad70fe559556f5ed5303
Sam Dension <sdenison@satx.rr.com>
2025-05-10 16:25:26 -0500

    SD - dynamo#45 - Test cases are working for password entropy.

---
+++ Source/Dynamo.Business.Shared/Cyber/Passwords/PasswordAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Dynamo.Business.Shared.Cyber.Passwords
+{
+    public class PasswordAnalyzer
+    {
+        public static double GetPasswordEntropy(string password)
+        {
+            var numberOfSymbols = password.ToCharArray().Distinct().Count();
+            var entropy = password.Count() * (Math.Log(numberOfSymbols) / Math.Log(2));
+            return entropy;
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/Cyber/PasswordAnalyzerTests.cs
@@ -0,0 +1,17 @@
+﻿using Dynamo.Business.Shared.Cyber.Passwords;
+using NUnit.Framework;
+
+namespace Dynamo.Business.Unit.Tests.Cyber
+{
+    public class PasswordAnalyzerTests
+    {
+        [TestCase("!TMYC2025!", 30)]
+        [TestCase("K9$pL!xQz&23", 43.02)]
+        public void Can_get_entropy_numbers_for_example_2(string password, double expectedEntropy)
+        {
+            var example2Entropy = PasswordAnalyzer.GetPasswordEntropy(password);
+            var tolerance = 0.001;
+            Assert.That(example2Entropy, Is.EqualTo(expectedEntropy).Within(tolerance));
+        }
+    }
+}
