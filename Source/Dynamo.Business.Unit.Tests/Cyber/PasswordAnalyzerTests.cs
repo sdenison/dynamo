7e477564b4865c1907076b62d2009dda915e0595
Sam Dension <sdenison@satx.rr.com>
2025-05-10 16:45:30 -0500

    SD - dynamo#45 - Solving 2025 spring week 4 part 1.

--- Source/Dynamo.Business.Unit.Tests/Cyber/PasswordAnalyzerTests.cs
+++ Source/Dynamo.Business.Unit.Tests/Cyber/PasswordAnalyzerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Dynamo.Business.Shared.Cyber.Passwords;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Dynamo.Business.Unit.Tests.Cyber
 {
@@ -12,6 +14,37 @@ namespace Dynamo.Business.Unit.Tests.Cyber
             var example2Entropy = PasswordAnalyzer.GetPasswordEntropy(password);
             var tolerance = 0.001;
             Assert.That(example2Entropy, Is.EqualTo(expectedEntropy).Within(tolerance));
+        }
+
+        [Test]
+        public void Can_solve_TMYC_2025_week_4_part_1()
+        {
+            var passwords = GetPasswordsFromFile("week4_pt1_input.txt");
+            Assert.That(passwords.Count, Is.EqualTo(200));
+            var entropyThreshold = 65;
+            var numberOfGoodPasswords = 0;
+            foreach (var password in passwords)
+            {
+                if (PasswordAnalyzer.GetPasswordEntropy(password) >= entropyThreshold)
+                    numberOfGoodPasswords++;
+            }
+            Assert.That(numberOfGoodPasswords, Is.EqualTo(81));
+        }
+
+        public List<string> GetPasswordsFromFile(string fileName)
+        {
+            var passwordFile = FileGetter.GetMemoryStreamFromFile(fileName);
+            var passwords = new List<string>();
+
+            using (var reader = new StreamReader(passwordFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    passwords.Add(line);
+                }
+            }
+            return passwords;
         }
     }
 }
