1b0e7040e418d6617400db9913788f61163c979f
Sam Dension <sdenison@satx.rr.com>
2025-05-15 19:09:03 -0500

    SD - dynamic#45 - Getting a list of spaces or double spaces

---
+++ Source/Dynamo.Business.Shared/Cyber/Stenography/MessageAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Dynamo.Business.Shared.Cyber.Stenography
+{
+    public class MessageAnalyzer
+    {
+        private char[] _punctuation = new char[]
+            {
+                '.',
+                ',',
+                '!',
+                '?',
+                ';',
+                ':'
+            };
+
+
+        public List<string> GetSpacesAfterPunctuation(string input)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsPunctuation(input[i]) && i + 1 < input.Length && input[i + 1] == ' ')
+                {
+                    // check for two spaces safely
+                    list.Add(i + 2 < input.Length && input[i + 2] == ' ' ? "  " : " ");
+                }
+            }
+
+            return list;
+        }
+
+        private bool IsPunctuation(char c)
+        {
+            foreach (var p in _punctuation)
+                if (p == c)
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ Source/Dynamo.Business.Unit.Tests/Cyber/StenographyTests.cs
@@ -0,0 +1,43 @@
+﻿using Dynamo.Business.Shared.Cyber.Stenography;
+using NUnit.Framework;
+using System.IO;
+using System.Text;
+
+namespace Dynamo.Business.Unit.Tests.Cyber
+{
+    public class StenographyTests
+    {
+        [Test]
+        public void Can_get_file_contents()
+        {
+            var fileContents = GetOneBigString("week5_part1_message.txt");
+            Assert.That(fileContents.StartsWith("security update: "));
+        }
+
+        [Test]
+        public void Can_parse_spaces()
+        {
+            var fileContents = GetOneBigString("week5_part1_message.txt");
+            var messageAnalyzer = new MessageAnalyzer();
+            var spaces = messageAnalyzer.GetSpacesAfterPunctuation(fileContents);
+            // The spaces are supposed to represent bytes so it should be divisible by 8
+            Assert.That(spaces.Count % 8, Is.EqualTo(0));
+
+        }
+
+
+        private static string GetOneBigString(string fileName)
+        {
+            var stream = FileGetter.GetMemoryStreamFromFile(fileName);
+            var sb = new StringBuilder();
+
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                    sb.Append(line.Trim());
+            }
+            return sb.ToString();
+        }
+    }
+}
